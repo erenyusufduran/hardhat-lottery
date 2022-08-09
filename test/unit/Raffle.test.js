818628c7c7d4611a2105af6514871081158cece6
Eren Yusuf Duran <erenyusufduran@gmail.com>
2022-08-10 02:27:37 +0300

    enterRaffle test

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -1,4 +1,4 @@
-const { assert } = require("chai");
+const { assert, expect } = require("chai");
 const { getNamedAccounts, deployments, ethers } = require("hardhat");
 const {
   developmentChains,
@@ -8,16 +8,17 @@ const {
 !developmentChains.includes(network.name)
   ? describe.skip
   : describe("Raffle Unit Tests", async function() {
-      let raffle, vrfCoordinatorV2Mock;
+      let raffle, vrfCoordinatorV2Mock, raffleEntranceFee, deployer;
       const chainId = network.config.chainId;
       beforeEach(async function() {
-        const { deployer } = await getNamedAccounts();
+        deployer = (await getNamedAccounts()).deployer;
         await deployments.fixture(["all"]);
         raffle = await ethers.getContract("Raffle", deployer);
         vrfCoordinatorV2Mock = await ethers.getContract(
           "VRFCoordinatorV2Mock",
           deployer
         );
+        raffleEntranceFee = await raffle.getEntranceFee();
       });
 
       describe("constructor", async function() {
@@ -26,6 +27,23 @@ const {
           const interval = await raffle.getInterval();
           assert.equal(raffleState.toString(), "0");
           assert.equal(interval.toString(), networkConfig[chainId]["interval"]);
+        });
+      });
+
+      describe("enterRaffle", async function() {
+        it("Reverts when you don't pay enough", async function() {
+          await expect(raffle.enterRaffle()).to.be.revertedWith(
+            "Raffle__NotEnoughETHEntered"
+          );
+        });
+        it("Raffle state must open", async function() {
+          const raffleState = await raffle.getRaffleState();
+          assert.equal(raffleState.toString(), "0");
+        });
+        it("Records players when they enter", async function() {
+          await raffle.enterRaffle({ value: raffleEntranceFee });
+          const playerFromContract = await raffle.getPlayer(0);
+          assert.equal(playerFromContract, deployer);
         });
       });
     });
