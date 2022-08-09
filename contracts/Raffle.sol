6bbad3d89c5377ebc249f80024e183a921d3f5ef
Eren Yusuf Duran <erenyusufduran@gmail.com>
2022-08-09 19:25:21 +0300

    event

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -7,6 +7,8 @@ contract Raffle {
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
 
+    event RaffleEnter(address indexed player);
+
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
@@ -14,6 +16,7 @@ contract Raffle {
     function enterRaffle() public payable {
         if (msg.value < i_entranceFee) revert Raffle__NotEnoughETHEntered();
         s_players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
     }
 
     function getEntranceFee() public view returns (uint256) {
